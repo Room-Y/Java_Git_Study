c53e4ca000ad388828853ccb6a0a9992e0b7e744
Java_Cmr <1018419943@qq.com>
2022-11-30 20:08:31 +0800

    new module day02_code

---
+++ day02_code/src/com/itheima/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.itheima.demo;
+
+/**
+ * @author: Java_cmr
+ * @Date: 2022/11/30 - 20:05
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello~");
+    }
+}
