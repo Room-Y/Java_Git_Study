67d01e59967af9e78aa5b3895fa480c9f121c407
Java_Cmr <1018419943@qq.com>
2022-12-02 19:29:33 +0800

    day06-day08

---
+++ day06_code/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author: Java_cmr
+ * @Date: ${DATE} - ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        playGame();
+    }
+
+    public static void playGame(){
+        System.out.println("play game！");
+    }
+}
---
+++ day07_code/src/test/Test1.java
@@ -0,0 +1,20 @@
+package test;
+
+/**
+ * @author: Java_cmr
+ * @Date: 2022/12/1 - 20:13
+ */
+public class Test1 {
+
+
+
+    public static void main(String[] args) {
+        int dad = 0;
+        dad++;
+        System.out.println(dad);
+        System.out.println(dad);
+        System.out.println(dad);
+        System.out.println(dad);
+
+    }
+}
